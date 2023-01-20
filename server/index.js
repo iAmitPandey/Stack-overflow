cc92edd0b05d446f927cd3b4b8a0032fbff12acf
Amit <amitpandey21599@gmail.com>
2023-01-20 13:25:28 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv"
 
 import userRoutes from "./routes/users.js";
 
 const app = express();
+dotenv.config();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
